7ab0bf146f9e181efab76c3ef71ba5f0f9e6b300
liwBh <liwbarqueroh@gmail.com>
2023-12-19 18:36:37 -0600

    avance en socket

--- chat_api/rooms/consumers.py
+++ chat_api/rooms/consumers.py
@@ -1,23 +1,31 @@
 import json
 import time
 
-from asgiref.sync import async_to_sync
+
 from channels.generic.websocket import WebsocketConsumer
 from django.core.exceptions import ValidationError
 
 
-class ProgressBarConsumer(WebsocketConsumer):
-        def connect(self):
-            self.accept()
+class chatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
 
-            # Enviar un mensaje de bienvenida
-            self.send(text_data=json.dumps({
-                'type': 'connect',
-                'message': 'Hola, mundo!'
-            }))
+        # Enviar un mensaje de bienvenida
+        self.send(text_data=json.dumps({
+            'type': 'connect',
+            'message': 'Socket conectado'
+        }))
 
-        def disconnect(self, close_code):
-            pass
+    def disconnect(self, close_code):
+        # Enviar un mensaje de despedidaA
+        self.send(text_data=json.dumps({
+            'type': 'disconnect',
+            'message': 'Socket desconectado!'
+        }))
+        
+        self.close()
+        
+            
 
-        def receive(self, text_data):
-            pass
+    def receive(self, text_data):
+        pass
