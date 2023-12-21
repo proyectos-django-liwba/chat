6dd83f6287aa98abd4863cbc77996e0118e13a36
Elmer Mejias <124623695+ing-Elmer@users.noreply.github.com>
2023-12-21 15:16:20 -0600

    agregada ruta

--- chat_api/chat_api/asgi.py
+++ chat_api/chat_api/asgi.py
@@ -7,19 +7,18 @@ from django.core.asgi import get_asgi_application
 
 from rooms.routing import rooms_websocket_urlpatterns
 from comments.routing import comments_websocket_urlpatterns
+
 django_asgi_app = get_asgi_application()
-
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chat_api.settings")
 
 application = ProtocolTypeRouter(
     {
-        "http": get_asgi_application(),
+        "http": django_asgi_app,
         "websocket": AuthMiddlewareStack(
             URLRouter(
-                rooms_websocket_urlpatterns
-            )
+                rooms_websocket_urlpatterns + comments_websocket_urlpatterns
+            ),
         ),
     }
 )
