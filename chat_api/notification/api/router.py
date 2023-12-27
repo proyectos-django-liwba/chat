04bf66d380bc1c7c4a74115515e060fbbd913f9f
Elmer Mejias <124623695+ing-Elmer@users.noreply.github.com>
2023-12-27 17:45:55 -0600

    arreglo ruta

--- chat_api/notification/api/router.py
+++ chat_api/notification/api/router.py
@@ -2,7 +2,7 @@ from django.urls import path
 from notification.api.views import NotificationListAPIView, NotificationDeleteApiView
 urlpatterns = [
     # Crear y registrar en una sala
-    path('notifications/<int:pk>/', NotificationDeleteApiView.as_view(), name='room'),
+    path('notification/<int:pk>/', NotificationDeleteApiView.as_view(), name='room'),
     path('notifications/', NotificationListAPIView.as_view(), name='room'),
     
 ]
