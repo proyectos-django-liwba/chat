490c7b3332469984164d159ad4313a004cb70cd1
Elmer Mejias <124623695+ing-Elmer@users.noreply.github.com>
2023-12-27 18:16:59 -0600

    cambios en modelo notification

--- chat_api/notification/models.py
+++ chat_api/notification/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from users.models import User
 from rooms.models import Room
 from django.db.models import CASCADE
-
+from django.db.models import SET_NULL
 class Notification(models.Model):
     CHOICES = (
         (1, 'Opción 1'),
@@ -11,7 +11,7 @@ class Notification(models.Model):
     )
     description = models.TextField(max_length=300)
     created_at = models.DateTimeField(auto_now_add=True)
-    room = models.ForeignKey(Room, on_delete=CASCADE, null=True)
+    room = models.ForeignKey(Room, on_delete=SET_NULL, null=True)
     user= models.ForeignKey(User, on_delete=CASCADE, null=True)
     users = models.ManyToManyField(User, related_name='notifications', blank=True)
     type = models.IntegerField(choices=CHOICES,default=0)
